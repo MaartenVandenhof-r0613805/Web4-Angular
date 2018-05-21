b5c410c5084a48190b482ea703f24dc97c1ac8fa
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2018-05-21 11:32:12 +0200

    next push

--- Web4-Angular/src/app/app.module.ts
+++ Web4-Angular/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { OverviewComponent } from './components/overview/overview.component';
+
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', component: FormComponent},
+  {path: 'overview', component: OverviewComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    OverviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
