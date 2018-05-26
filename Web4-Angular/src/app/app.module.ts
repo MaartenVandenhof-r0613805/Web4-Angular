ccbbddbbf45210e6a07d51f4b5a2b0188a51a7fc
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2018-05-26 17:05:01 +0200

    Angular Project done

--- Web4-Angular/src/app/app.module.ts
+++ Web4-Angular/src/app/app.module.ts
@@ -2,21 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './components/form/form.component';
 import { OverviewComponent } from './components/overview/overview.component';
 
 import {RouterModule, Routes} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 
 const appRoutes: Routes = [
-  {path: '', component: FormComponent},
-  {path: 'overview', component: OverviewComponent}
+  {path: '', component: OverviewComponent},
+
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
     OverviewComponent
   ],
   imports: [
